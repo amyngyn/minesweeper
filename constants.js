ccd2f7f385eff70986384e21a2ee1ff7fed730ae
Amy Nguyen <amynguyen93@gmail.com>
2014-12-18 18:13:00 -0800

    updated constants

--- constants.js
+++ constants.js
@@ -1,1 +1,21 @@
+minesweeper = {};
 minesweeper.MINE = 'X';
+minesweeper.FLAG = 'X';
+minesweeper.SETTINGS = {
+  BEGINNER: {
+    rows: 8,
+    cols: 8,
+    mines: 10 
+  },
+  INTERMEDIATE: {
+    rows: 16,
+    cols: 16,
+    mines: 40
+  },
+  EXPERT: {
+    rows: 16,
+    cols: 30,
+    mines: 99
+  }
+};
+
